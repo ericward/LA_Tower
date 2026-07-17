b6998903c91bd1448e179b925f6dfa21dfd268c3
Eric J. Ward <eric.ward@gmail.com>
2026-07-17 16:18:37 -0400

    Update Annual_Summary.R
    updated carbon units

--- Annual_Summary.R
+++ Annual_Summary.R
@@ -1,8 +1,9 @@
 library(dplyr)
 library(tidyr)
 library(gt)
+library(lubridate)
 
-setwd("~/GitHub/LA_Tower/Data")
+setwd("C:/Users/ejward2/OneDrive - NASA/Desktop/Tower Data/Melinda/LA_Tower/Data")
 
 list.files(,pattern="csv")
 
@@ -56,12 +57,20 @@ la2_annual_summary <- la2 %>%
 la2_annual_summary
 
 # Cumulative Summary by Year 
+mwCH4<-16.043 #g/mol
+nmol2gCH4<-1/(10^6*mwCH4)
+mwCO2<-44.009 #g/mol
+mmol2gCH4<-1/(10^3*mwCO2)
+d2s<-(24*60*60) #seconds per day
+ucCH4<-nmol2gCH4*d2s #nmol m-2 s-1 to g m-2 y-1
+ucCO2<-mmol2gCH4*d2s #nmol m-2 s-1 to g m-2 y-1
+
 la3_annual_cumulative <- la3 %>%
   mutate(Year = year(Date)) %>%
   group_by(Year) %>%
   summarise(
-    CO2_cumulative = sum(FC_filled, na.rm = TRUE),
-    CH4_cumulative = sum(FCH4_filled, na.rm = TRUE),
+    CO2_cumulative = sum(FC_filled, na.rm = TRUE)*ucCO2,
+    CH4_cumulative = sum(FCH4_filled, na.rm = TRUE)*ucCH4,
     LE_cumulative  = sum(LE_filled, na.rm = TRUE),
     Days = n()
   )
@@ -72,8 +81,8 @@ la2_annual_cumulative <- la2 %>%
   mutate(Year = year(Date)) %>%
   group_by(Year) %>%
   summarise(
-    CO2_cumulative = sum(FC_filled, na.rm = TRUE),
-    CH4_cumulative = sum(FCH4_filled, na.rm = TRUE),
+    CO2_cumulative = sum(FC_filled, na.rm = TRUE)*ucCO2,
+    CH4_cumulative = sum(FCH4_filled, na.rm = TRUE)*ucCH4,
     LE_cumulative  = sum(LE_filled, na.rm = TRUE),
     Days = n()
   )
